962e0699a5ab7fd0d5c235d1563121d71a874834
mdrezuanislamridoy <mdrezuanislamridoy@gmail.com>
2025-08-30 16:34:19 +0600

    first commit

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3030/api",
+  baseURL: "digital-sonod-server-l6m5386sn.vercel.app/api",
   withCredentials: true,
 });
 
