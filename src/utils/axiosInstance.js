832df55c1c978922a798f75efcbce47ab6c598d7
mdrezuanislamridoy <mdrezuanislamridoy@gmail.com>
2025-09-09 17:32:30 +0600

    url changed

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:3030/api"; //https://digital-sonod-server.vercel.app/api
+const baseURL = "https://digital-sonod-server.vercel.app/api"; // "http://localhost:3030/api";
 const axiosInstance = axios.create({
   baseURL: baseURL,
   withCredentials: true,
